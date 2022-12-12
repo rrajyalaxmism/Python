5e650d3a398dcc22836fde0856a156844520b179
rrajyalaxmism <61182796+rrajyalaxmism@users.noreply.github.com>
2022-12-12 16:57:21 +0530

    more list functions

---
+++ 09_More_Lists.py
@@ -0,0 +1,59 @@
+#basic
+print(len([1,2,3,4]))
+print([1,2,3]+[4,5,6])
+print(['NI']*4)
+
+print(str([1,2])+'34')
+print([1,2]+list("34"))
+
+#List comprehensions-------------
+r=[c*4 for c in 'SpAm']
+print(r)
+
+print(list(map(abs,[-1,-2,0,1,2])))
+
+#----------Slicing -------------
+
+l=[[1,2,3],[4,5,6],[7,8,9]]
+print(l[2])
+print(l[-2])
+print(l[1:])
+print(l[1][2])
+
+#---------few inbuilt functions----------
+L=['eat','more','eggs']
+L[1:3]=['less','carbs']
+print(L)
+L.append(':)')
+print(L)
+L.sort()
+print(L)
+Li=[1,2,3,4]
+Li[len(Li):]=[5,6,7]
+print(Li)
+Li.extend([8,9,10])
+print(Li)
+
+LS=['abc','ABC','Xyz','xYZ']
+LS.sort(key=str.lower,reverse=True)
+print(LS)
+LS=['abc','ABC','Xyz','xYZ']
+print(sorted(LS,key=str.lower,reverse=True))
+print(sorted([x.lower() for x in LS],reverse=True))
+
+Li.reverse()
+print(Li)
+print(reversed(Li))
+print(list(reversed(Li)))
+
+L = ['spam', 'eggs', 'ham', 'toast']
+del L[0] # Delete one item
+print(L)#['eggs', 'ham', 'toast']
+del L[1:] # Delete an entire section
+print(L) # Same as L[1:] = [] #['eggs']
+
+L = ['Already', 'got', 'one']
+L[1:] = []
+print(L)#['Already']
+L[0] = []
+print(L)#[[]]
